83645b55a26923715de8e336dd09fa9e38d269bb
Pragyadipta Pradhan <22053612@kiit.ac.in>
2025-03-21 08:49:59 +0530

    Adding lab 8 and 9

---
+++ LAB 9/prog2/generate_xml.php
@@ -0,0 +1,9 @@
+<?php
+header("Content-Type: text/xml");
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo "<note>";
+echo "<from>jani</from>";
+echo "<to>tove</to>";
+echo "<message>Hello</message>";
+echo "</note>";
+?>
